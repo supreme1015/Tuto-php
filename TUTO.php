7051b71a5cdf482f0ae7985f7995e10041c60b33
supreme1015 <mamoudoudjiboabdoulkader004@gmail.com>
2022-06-02 17:29:53 +0100

    first commit

---
+++ TUTO.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>registration form</title>
+</head>
+<h1> Blood donation </h1>
+<body bgcolor="FBB917">
+    <div><h2>registration form</h2></div>
+    <form action='connect.php' method="POST">
+        <label for="user">Name:</label><br>
+        <input type='text' name='name' id="name" required> <br> <br>
+       
+        
+        <label for="email">Email: <label> <br>
+        <input type='email' name='email' id="email" required> <br> <br>
+
+        <label for="phone">phone: <label> <br>
+        <input type='text' name='phone' id="phone" required> <br> <br>
+
+        <label for="bgroup">Blood Group: <label> <br>
+        <input type='text' name='bgroup' id="bgroup" required> <br> <br>
+
+        <input type='submit' name='submit' id="submit"/>
+    </from>
+</body>
+</html>
